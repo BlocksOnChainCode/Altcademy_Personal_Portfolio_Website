654d9ca73d6d9a7ba37fd8ec03c9627cb6d5bfe4
Arno luyckx <A.luyckx@protonmail.com>
2022-09-30 20:11:49 +0200

    Created basic header, started on portfolio pieces bootstrap cards

---
+++ main.js
@@ -0,0 +1,43 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Current Project: Portfolio Website', 'success')
+  })
+}
+
+/*------------------------------*/
+
+//Google charts for tech stacks used
+// Load google charts
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['HTML', 8],
+  ['CSS', 2],
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'width':400, 'height':200};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
